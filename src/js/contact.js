40c35d0e3c8967c16610dcd25381e55d0026bbef
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-02-24 22:33:37 +0100

    changed contact.html and js

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,3 +1,51 @@
-//linke riktig, feks consollog for å se at de prater med hverandrew
-//document.querySelector get element by id, dobbelt sjekk at html elementer har id
-//sett opp eventlistener
+const formEl = document.querySelector("#js-form");
+const nameEl = document.querySelector("#js-name");
+const emailEl = document.querySelector("#js-email");
+const adressEl = document.querySelector("#js-adress");
+
+formEl.addEventListener ("submit", (event) => {
+    event.preventDefault();
+
+    const name = nameEl.value;
+    const email = emailEl.value;
+    const adress = adressEl.value;
+    const message = messageEl.value;
+
+    if (name === "") {
+        alert("please enter a name");
+        return;
+    }
+
+    if (email === "") {
+        alert("please enter an email-adress");
+        return;
+    }
+
+    if (adress === "") {
+        alert("please enter an adress");
+        return;
+    }
+
+    if (subject === "") {
+        alert("please enter a message");
+        return;
+    }
+
+    alert("form submitted")
+ 
+});
+
+nameEl.addEventListener("blur", (event) => {
+    const name = event.target.value.trim();
+
+    const minLengthRegex = /^[a-zA-Z]{2,}/;
+
+    if (minLengthRegex.test(name)) {
+     event.target.classList.add("is-success");
+     event.target.classList.remove("is-wrong");
+    }
+    else {
+    event.target.classList.add("is-wrong");
+    event.target.classlist.remove("is-success");
+    }
+});
