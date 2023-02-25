d5a97b67aa63d6a47a1918385f5f0d04c9a86ac0
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2023-02-25 18:32:16 +0100

    changed js

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,51 +1,65 @@
-const formEl = document.querySelector("#js-form");
-const nameEl = document.querySelector("#js-name");
-const emailEl = document.querySelector("#js-email");
-const adressEl = document.querySelector("#js-adress");
+var myForm = document.querySelector("#js-form-contact");
+myForm.addEventListener("submit", checkFields);
+function checkFields(event) {
+  event.preventDefault();
+  var is_all_ok = true;
+  let nameRegEx = /^[a-z ,.'-]+$/i;
+  const emailEl = document.querySelector("#js-email");
+  const adressEl = document.querySelector("#js-adress");
+  const nameEl = document.querySelector("#js-name");
+  const birthDateEl = document.querySelector("#js-birthday");
 
-formEl.addEventListener ("submit", (event) => {
-    event.preventDefault();
+  const name = nameEl.value;
+  const email = emailEl.value;-
+  const adress = adressEl.value;
+  const birthday = birthDateEl.value;
+  resetValidationFeedback();
+  console.log(birthday);
 
-    const name = nameEl.value;
-    const email = emailEl.value;
-    const adress = adressEl.value;
-    const message = messageEl.value;
+  if (name === "" || !nameRegEx.test(name)) {
+    document.getElementById("nameerrormsg").style.display = "inline";
+    is_all_ok = false;
+  }
 
-    if (name === "") {
-        alert("please enter a name");
-        return;
+  if (email === "") {
+    console.log(email);
+    document.getElementById("mailerrormsg").style.display = "inline";
+    is_all_ok = false;
+  }
+  if (adress === "") {
+    document.getElementById("addresserrormsg").style.display = "inline";
+    is_all_ok = false;
+  }
+
+  if (birthday === "") {
+    document.getElementById("dateerrormsg").style.display = "inline";
+    is_all_ok = false;
+  } else {
+    var dateProvided = new Date(birthday);
+    var dateToday = new Date();
+    if (
+      dateProvided > dateToday ||
+      dateProvided.toDateString() == dateToday.toDateString()
+    ) {
+      is_all_ok = false;
+      document.getElementById("dateerrormsg").style.display = "inline";
+      document.getElementById("dateerrormsg").textContent +=
+        " Date needs to be a date before the current date";
+    } else if (dateToday.getFullYear() - dateProvided.getFullYear() > 100) {
+      is_all_ok = false;
+      document.getElementById("dateerrormsg").style.display = "inline";
+      document.getElementById("dateerrormsg").textContent +=
+        " You cannot be more than 100 years old.";
     }
+  }
+  return is_all_ok;
+}
 
-    if (email === "") {
-        alert("please enter an email-adress");
-        return;
-    }
-
-    if (adress === "") {
-        alert("please enter an adress");
-        return;
-    }
-
-    if (subject === "") {
-        alert("please enter a message");
-        return;
-    }
-
-    alert("form submitted")
- 
-});
-
-nameEl.addEventListener("blur", (event) => {
-    const name = event.target.value.trim();
-
-    const minLengthRegex = /^[a-zA-Z]{2,}/;
-
-    if (minLengthRegex.test(name)) {
-     event.target.classList.add("is-success");
-     event.target.classList.remove("is-wrong");
-    }
-    else {
-    event.target.classList.add("is-wrong");
-    event.target.classlist.remove("is-success");
-    }
-});
+function resetValidationFeedback() {
+  document.getElementById("nameerrormsg").style.display = "none";
+  document.getElementById("mailerrormsg").style.display = "none";
+  document.getElementById("addresserrormsg").style.display = "none";
+  document.getElementById("dateerrormsg").style.display = "none";
+  document.getElementById("dateerrormsg").textContent =
+    "Please enter a valid birth date.";
+}
